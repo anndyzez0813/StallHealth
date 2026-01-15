72ac09a0dde6a7771b50909781accc0849bc9b7f
anndyzez0813 <54787370+anndyzez0813@users.noreply.github.com>
2026-01-15 12:10:52 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,15 @@
-
-```javascript
-const CACHE_NAME = 'stallhealth-v1';
+const CACHE_NAME = 'stall-health-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -22,6 +19,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Cache hit - return response
         if (response) {
           return response;
         }
@@ -30,3 +28,17 @@ self.addEventListener('fetch', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
